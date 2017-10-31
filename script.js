dd3212cba385ffeccb3f6dd1e74836076ea863f2
Brad Hansen <brad.m.hansen@gmail.com>
2017-10-31 23:39:31 +1300

    Changes to how locations are found + other small changes

--- script.js
+++ script.js
@@ -12,11 +12,17 @@ function init() {
 }
 
 function randomStreetView(callback) {
+  // 100km search radius is a balance between it being too small and looping 
+  // many times to find land or it being too large and always snapping to 
+  // islands in the middle of oceans (although it does still do this often)
+  var snaptoRadiusMeters = 100e3;
   var sv = new google.maps.StreetViewService();
-  //var randLat = (Math.random() * 180) - 90;
-  var randLat = Math.random() < 0.5 ? -(Math.random()**3)*65 : (Math.random()**3)*65;
+  // Random value from 1 to -1, cubed to make most values closer to 0 and keep 
+  // negative values negative, mapped onto 65 degrees of latitude because 
+  // there is boring stuff in the top/bottom 25 degrees of world.
+  var randLat = ((Math.random()*2 - 1)**3)*65;
   var randLng = (Math.random() * 360) - 90;
-  sv.getPanorama({location: {lat: randLat, lng: randLng}, radius: 100000000}, callback);
+  sv.getPanorama({location: {lat: randLat, lng: randLng}, radius: snaptoRadiusMeters}, callback);
 }
 
 function processSVData(data, status) {
@@ -24,16 +30,16 @@ function processSVData(data, status) {
     panorama.setPano(data.location.pano);
     panorama.setPov({
       heading: 270,
-      pitch: 0
+      pitch: 0,
     });
     panorama.setVisible(true);
   } else {
-    //try another location
+    //try another location, try parallel requests later maybe?
 	randomStreetView(processSVData);
   }
 }
 
-//cleanup
+//dirty cleanup
 window.onload = function () {
   document.getElementsByClassName("gmnoprint gm-style-cc")[0].remove();
   document.getElementsByClassName("gm-style-cc")[1].remove();
