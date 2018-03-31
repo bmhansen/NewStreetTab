bd55be175861f4c5d0c491e406f606ca91b23721
Brad Hansen <brad.m.hansen@gmail.com>
2018-04-01 11:06:51 +1200

    Adds automatic rotation to the panorama

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 var panorama;
 function init() {
   panorama = new google.maps.StreetViewPanorama(document.getElementById('street-view'),
-  {
-    pov: {heading: Math.floor(Math.random() * 360), pitch: 0},
-    zoom: .5,
-	disableDefaultUI: true,
-	addressControl: true,
-  });
+    {
+      pov: { heading: Math.floor(Math.random() * 360), pitch: 0 },
+      zoom: .5,
+      disableDefaultUI: true,
+      addressControl: true,
+    });
   // attempts to use a locally stored panorama, fetches new one if not found
   chrome.storage.sync.get("panoData", processLocalFetch);
+  asyncRotatePano();
   //fetches another panorama for next time a tab is opened
   randomStreetView(storePano);
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function asyncRotatePano() {
+  let j = (Math.random() < 0.5) ? 1 : -1;
+  let heading = panorama.getPov().heading;
+  while (heading == panorama.getPov().heading) {
+    heading += j / 16;
+    panorama.setPov({
+      heading: heading,
+      pitch: 0
+    });
+    await sleep(8);
+  }
+}
+
 function processLocalFetch(data) {
   // if there is no locally stored panorama then find one and use it
-  if (Object.keys(data).length === 0 && data.constructor === Object){
+  if (Object.keys(data).length === 0 && data.constructor === Object) {
     randomStreetView(usePano);
   }
   panorama.setPano(data.panoData);
@@ -29,19 +47,18 @@ function randomStreetView(callback) {
   // Random value from 1 to -1, cubed to make most values closer to 0 and to
   // keep negative values negative, mapped onto 65 degrees of latitude 
   // because there is boring stuff in the top/bottom 25 degrees of world.
-  var randLat = ((Math.random()*2 - 1)**3)*65;
+  var randLat = ((Math.random() * 2 - 1) ** 3) * 65;
   //random longitude 0-360 degrees
   var randLng = (Math.random() * 360) - 180;
-  sv.getPanorama({location: {lat: randLat, lng: randLng}, radius: snaptoRadiusMeters}, callback);
+  sv.getPanorama({ location: { lat: randLat, lng: randLng }, radius: snaptoRadiusMeters }, callback);
 }
 
 function storePano(data, status) {
   if (status === "OK") {
-    console.log(data.location.pano);
-    chrome.storage.sync.set({"panoData": data.location.pano}, );
+    chrome.storage.sync.set({ "panoData": data.location.pano }, );
   } else {
     //try another location, try parallel requests later maybe?
-	randomStreetView(storePano);
+    randomStreetView(storePano);
   }
 }
 
@@ -50,11 +67,11 @@ function usePano(data, status) {
     panorama.setPano(data.location.pano);
   } else {
     //try another location, try parallel requests later maybe?
-	randomStreetView(usePano);
+    randomStreetView(usePano);
   }
 }
 
-//dirty cleanup
+//hacky cleanup
 window.onload = function () {
   document.getElementsByClassName("gmnoprint gm-style-cc")[0].remove();
   document.getElementsByClassName("gm-style-cc")[1].remove();
